b349d58079f4f1cc2c325546178f92772ee4cd36
Quang Vo <thanhquang2608@gmail.com>
2021-08-28 21:28:43 +0700

    feat: Implement more snippets and commands

--- extension.js
+++ extension.js
@@ -1,5 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
+const { Position } = require("vscode");
 const vscode = require("vscode");
 
 // this method is called when your extension is activated
@@ -9,11 +10,31 @@ const vscode = require("vscode");
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with  registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "boostcode.sum",
+  const sumCmd = vscode.commands.registerCommand("boostcode.sum", function () {
+    // The code you place here will be executed every time your command is executed
+    // Get the active text editor
+    const editor = vscode.window.activeTextEditor;
+    if (!editor) return;
+
+    const document = editor.document;
+    const selection = editor.selection;
+
+    // Get the word within the selection
+    const lines = document.getText(selection).split("\n");
+    const sum = lines.reduce((acc, value) => {
+      const num = +value;
+      if (isNaN(num)) return acc;
+      return acc + num;
+    }, 0);
+
+    // Show the result
+    editor.edit((editBuilder) => {
+      editBuilder.insert(selection.end, `\nSum: ${sum}`);
+    });
+  });
+
+  const linesToArrayCmd = vscode.commands.registerCommand(
+    "boostcode.lines-to-array",
     function () {
       // The code you place here will be executed every time your command is executed
       // Get the active text editor
@@ -24,21 +45,36 @@ function activate(context) {
       const selection = editor.selection;
 
       // Get the word within the selection
-      const lines = document.getText(selection).split("\n");
-      const sum = lines.reduce((acc, value) => {
-        const num = +value;
-        if (isNaN(num)) return acc;
-        return acc + num;
-      }, 0);
-
-      // Show the result
-      editor.edit((editBuilder) => {
-        editBuilder.insert(selection.end, `\nSum: ${sum}`);
+      let lines = document.getText(selection).split("\n");
+      if (lines[lines.length - 1] === "") {
+        lines.pop();
+      }
+      const array = lines.map((line, index) => {
+        let ret = `\t"${line.replace(/"/g, '\\"')}"`;
+        if (index != lines.length - 1) {
+          ret += ",";
+        }
+        return ret;
       });
+
+      const position = editor.selection.end;
+      const start = new Position(position.line + 1, 0);
+      // Show the result
+      editor
+        .edit((editBuilder) => {
+          editBuilder.insert(selection.end, "\n");
+          editBuilder.insert(selection.end, "[\n");
+          editBuilder.insert(selection.end, array.join("\n"));
+          editBuilder.insert(selection.end, "\n]");
+        })
+        .then((success) => {
+          console.log("success:", success);
+          editor.selection = new vscode.Selection(start, editor.selection.end);
+        });
     }
   );
 
-  context.subscriptions.push(disposable);
+  context.subscriptions.push(sumCmd, linesToArrayCmd);
 }
 
 // this method is called when your extension is deactivated
